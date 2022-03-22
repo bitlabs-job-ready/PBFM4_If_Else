22c6ba1cb535b9511036cf4b0eecec3baccad057
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-22 18:10:11 +0530

    Add files via upload

---
+++ PBFM4_If_Else.py
@@ -0,0 +1,10 @@
+# if-else
+# Write a Python Program the Check whether Three variables are Equal or Not
+
+#input
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+c=int(input("Enter c value:"))
+
+# Condition
+
